2d7e66f8cc09226e267042363e240f589c4a7fda
Visar Sabani <vissarshabani@gmail.com>
2023-11-17 11:13:25 +0000

    Lists added

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+  get "lists/:id", to: "lists#show"
+  # Rails.application.routes.draw do
+  #   resources :lists do
+  #     resources :bookmarks, only: [:new, :create]
+  #   end
+  #   resources :bookmarks, only: [:destroy]
+  # end
 end
